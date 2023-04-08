967f56f7b72d47745e28f523beb2d6aa6599a3a4
janekk <janekkamon@gmail.com>
2023-04-08 20:48:04 +0200

    toggle completed

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -5,17 +5,20 @@ import AddTodoForm from '../AddTodoForm/AddTodoForm';
 import TodosList from '../TodosList/TodosList';
 import TodosOptions from '../TodosOptions/TodosOptions';
 import Footer from '../Footer/Footer';
+import { ThemeProvider } from '../../ThemeContext';
 
 export default function App() {
   return (
-    <div id="container">
-      <Header />
-        <main>
-          <AddTodoForm />
-          <TodosList />
-          <TodosOptions />
-        </main>
-      <Footer />
-    </div>
+    <ThemeProvider>
+      <div id="container">
+        <Header />
+          <main>
+            <AddTodoForm />
+            <TodosList />
+            <TodosOptions />
+          </main>
+        <Footer />
+      </div>
+    </ThemeProvider>
   )
 }
--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,11 +1,15 @@
 import React from 'react'
 import './Header.css';
+import { useTheme, useThemeUpdate } from '../../ThemeContext';
 
 export default function Header() {
+  const toggleTheme = useThemeUpdate();
+  const themeStyle = useTheme();
+
   return (
     <header>
-      <h1>TODO</h1>
-      <span></span>
+      <h1 className={themeStyle}>TODO</h1>
+      <span className={themeStyle} onClick={toggleTheme}></span>
     </header>
   )
 }
