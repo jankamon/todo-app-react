319d55021d8488a3bc235f564bbfbbdab2d71862
janekk <janekkamon@gmail.com>
2023-04-09 14:48:29 +0200

    wip todos list

--- src/Components/TodosOptions/TodosOptions.js
+++ src/Components/TodosOptions/TodosOptions.js
@@ -1,7 +1,19 @@
-import React from 'react'
+import React from 'react';
+import './TodosOptions.css';
+import { useTheme } from '../../ThemeContext';
 
 export default function TodosOptions() {
+  const themeStyle = useTheme();
+
   return (
-    <div>TodosOptions</div>
+    <div id='todos-options' className={themeStyle}>
+      <div id='items-left'>5 items left</div>
+      <div id='filter-options'>
+        <div>All</div>
+        <div>Active</div>
+        <div>Completed</div>
+      </div>
+      <div id='clear-completed'>Clear Completed</div>
+    </div>
   )
 }
