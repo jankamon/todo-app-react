2be07cf87b01a3ea43f75a6d6fa17deed1bcf099
janekk <janekkamon@gmail.com>
2023-04-09 14:11:56 +0200

    todo list

--- src/Components/Todos/Todos.js
+++ src/Components/Todos/Todos.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import './Todos.css';
+import { useTheme } from '../../ThemeContext';
 
 export default function TodosList() {
+  const themeStyle = useTheme();
+  
   return (
     <ul>
-      <li>Todo item</li>
-      <li>Todo item</li>
-      <li>Todo item</li>
+      <li className={themeStyle}><span className="circle todo-button"></span><span className='todo-text'>Todo item</span><span className="remove-button"></span></li>
+      <li className={themeStyle}><span className="circle todo-button"></span><span className='todo-text'>Todo item</span><span className="remove-button"></span></li>
+      <li className={themeStyle}><span className="circle todo-button"></span><span className='todo-text'>Todo item</span><span className="remove-button"></span></li>
     </ul>
   )
 }
