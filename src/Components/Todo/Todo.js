924f8fbcd1899a8fe5571b8b7e778fe9feef1b25
janekk <janekkamon@gmail.com>
2023-04-11 15:51:46 +0200

    added check icon if completed

--- src/Components/Todo/Todo.js
+++ src/Components/Todo/Todo.js
@@ -1,6 +1,7 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import './Todo.css';
 import { useTheme } from '../../ThemeContext';
+import iconCheck from './images/icon-check.svg'
 
 export default function Todo({ todo, removeTodo, toggleTodo }) {
     const themeStyle = useTheme();
@@ -8,10 +9,10 @@ export default function Todo({ todo, removeTodo, toggleTodo }) {
     return (
         <li className={themeStyle}>
             <span 
-                onClick={() => toggleTodo(todo.id)
-                }
-                className="circle todo-button" >
-                <span className='inner-circle'></span>
+                onClick={() => toggleTodo(todo.id)}
+                className={`circle todo-button + ${todo.complete ? 'complete' : ''}`} 
+            >
+                {todo.complete ? <img src={iconCheck} alt='check icon' /> : ''}
             </span>
             <span className={`todo-text + ${todo.complete ? 'complete' : ''}`}>{todo.name}</span>
             <span 
